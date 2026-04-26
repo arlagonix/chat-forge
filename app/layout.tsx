4d6b47bd971b0a26742b3a1f44dc80432581292b
Alexander Gorbunov <gorbunov-sasha@yandex.ru>
2026-04-27 02:35:01 +0300

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
@@ -31,6 +32,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster position="bottom-right" />
         </ThemeProvider>
       </body>
     </html>
